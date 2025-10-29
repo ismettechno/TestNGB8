1d72309f026a8d79af2c5efb3c6ebd958ca8f567
TechnoStudy <ismettechno@gmail.com>
2025-10-29 19:34:57 +0000

    Initial commit

---
+++ src/Gun01/_01_Giris.java
@@ -0,0 +1,49 @@
+package Gun01;
+
+import org.testng.annotations.Test;
+
+public class _01_Giris {
+
+//   public static void main(String[] args) {
+//        driverIslemleriniYap();
+//        websitesiniAc();
+//        LoginTestiniYap()
+//        DriverKapat();
+//   }
+
+//      @Test JUnit Test i
+//      public void Test1()
+//      {
+//          driverIslemleriniYap();
+//          websitesiniAc();
+//          LoginTestiniYap()
+//          Assert test test
+//          DriverKapat();
+//      }
+
+
+    @Test
+    public void DriverIslemleriniYap()
+    {
+        System.out.println("Driver Başlat çalıştı");
+    }
+
+    @Test
+    public void WebsitesiniAc()
+    {
+        System.out.println("WebsitesiniAc çalıştı");
+    }
+
+    @Test
+    public void LoginTestiniYap()
+    {
+        System.out.println("LoginTestiniYap çalıştı");
+    }
+
+    @Test
+    public void DriverKapat()
+    {
+        System.out.println("Driver Kapat çalıştı");
+    }
+
+}
