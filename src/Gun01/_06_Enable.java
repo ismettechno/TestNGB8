c9abeb5ce1319324ba164ac4248b2a7871706688
TechnoStudy <ismettechno@gmail.com>
2025-10-29 20:20:54 +0000

    BaseDriver

---
+++ src/Gun01/_06_Enable.java
@@ -0,0 +1,26 @@
+package Gun01;
+
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class _06_Enable extends BaseDriver {
+    @Test
+    public void Test1()
+    {
+        System.out.println("Test1");
+        driver.get("https://www.facebook.com/");
+    }
+
+    @Test(enabled = false)
+    public void Test2()
+    {
+        System.out.println("Test2");
+    }
+
+    @Test
+    public void Test3()
+    {
+        System.out.println("Test3");
+        driver.get("https://www.google.com/");
+    }
+}
