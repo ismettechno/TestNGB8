ea0dbd2adf6c1f59b9a3287dc7c3a3cddea0aceb
TechnoStudy <ismettechno@gmail.com>
2025-10-30 19:25:23 +0000

    SubscribeFunctionYes

---
+++ src/Gun02/_01_SubscribeNewsLetter.java
@@ -0,0 +1,32 @@
+package Gun02;
+
+import Utility.BaseDriver;
+import Utility.WebTool;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _01_SubscribeNewsLetter extends BaseDriver {
+     /*
+    Senaryo
+  1- Siteyi açınız.
+  2- Newsletter  Subscribe işlemini abone olunuz(YES)  , işlemin başarılı olduğunu kontrol ediniz.
+     */
+
+    @Test
+    public void SubscribeFunctionYes() {
+        WebElement newsLetterLink=driver.findElement(By.linkText("Newsletter"));
+        newsLetterLink.click();
+
+        WebElement subscribeYes=driver.findElement(By.xpath("//input[@type='radio' and @value='1']"));
+        subscribeYes.click();
+
+        WebElement continueButton=driver.findElement(By.xpath("//*[@value='Continue']"));
+        continueButton.click();
+
+        WebTool.SuccessMessageValidation();
+    }
+
+
+}
