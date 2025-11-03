e9ec723a98681ca125f475010f1f38a13d087fe3
TechnoStudy <ismettechno@gmail.com>
2025-11-03 21:13:00 +0000

    _01_Soru

---
+++ src/Gun04/_01_Soru.java
@@ -0,0 +1,13 @@
+package Gun04;
+
+/*
+
+   Senaryo
+   1- Siteye gidiniz
+   2- Specials tiklayiniz
+   3- Cikan urun lerde (bütün ürünlerde indirim var mı, eski fiyat yeni fiyat var mı)
+   4- Yukarıdaki işlemden sonra her ürünün önceki fiyatının şu andaki fiyattan
+      yüksek olduğunu doğrulayınız.
+*/
+public class _01_Soru {
+}
