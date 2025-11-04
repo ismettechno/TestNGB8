24d6e94adc71fbd7c37e51e3513c1c46522e3619
TechnoStudy <ismettechno@gmail.com>
2025-11-04 19:46:37 +0000

    SpecialsTest

--- src/Gun04/_01_Soru.java
+++ src/Gun04/_01_Soru.java
@@ -1,5 +1,13 @@
 package Gun04;
 
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 /*
 
    Senaryo
@@ -9,5 +17,25 @@ package Gun04;
    4- Yukarıdaki işlemden sonra her ürünün önceki fiyatının şu andaki fiyattan
       yüksek olduğunu doğrulayınız.
 */
-public class _01_Soru {
+public class _01_Soru extends BaseDriver {
+
+    @Test
+    public void SpecialsTest()
+    {
+        WebElement specials=driver.findElement(By.linkText("Specials"));
+        specials.click();
+
+        List<WebElement> newPrices=driver.findElements(By.cssSelector("span[class='price-new']"));
+        List<WebElement> oldPrices=driver.findElements(By.cssSelector("span[class='price-old']"));
+
+        Assert.assertTrue(newPrices.size() == oldPrices.size(),"Bütün ürünlerde indirim bulunamadı");
+
+        for (int i = 0; i < newPrices.size(); i++) {
+            double newPrice=Double.parseDouble(newPrices.get(i).getText().replaceAll("[^0-9,.]","") );
+            double oldPrice=Double.parseDouble(oldPrices.get(i).getText().replaceAll("[^0-9,.]","") );
+
+            Assert.assertTrue(oldPrice>newPrice,"Yeni fiyat eskisinden küçük değil");
+        }
+
+    }
 }
