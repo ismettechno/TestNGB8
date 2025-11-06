9a640d298857f2ae80876940d1cb9318b0e3fb7e
TechnoStudy <ismettechno@gmail.com>
2025-11-06 20:48:34 +0000

    Loglama

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,5 +1,7 @@
 package Utility;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,19 +9,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class BaseDriver {
+    public static Logger LogEkle= LogManager.getLogger();// Logları ekleyeceğim nesneyi başlattım.
+
     public static WebDriver driver;
     public static WebDriverWait bekle;
 
     @BeforeClass
     public void Setup()
     {
+        LogEkle.info("Log Tutma işlemi başladı");
         driver = new ChromeDriver();
 
         driver.manage().window().maximize(); // Ekranı max yapıyor.
@@ -27,6 +34,7 @@ public class BaseDriver {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // 5 sn mühlet: elementi bulma mühleti
 
         bekle =new WebDriverWait(driver, Duration.ofSeconds(20));
+        LogEkle.info("Başlangıç değişkenleri driver,wait,log tanımlandı ve başlatıldı");
         LoginIn();
     }
 
@@ -35,11 +43,14 @@ public class BaseDriver {
     {
         MyFunc.Bekle(5);
         driver.quit();  // açılmış bütün tabları kapatır, tarayıcıyı kapatır
+
+        LogEkle.warn("Driver kapatılamadı");
     }
 
     public void LoginIn()
     {
         System.out.println("Login Testi başladı");
+        LogEkle.info("Login Test başladı");
         driver.get("https://opencart.abstracta.us/index.php?route=account/login");
 
         WebElement email=driver.findElement(By.xpath("//input[@id='input-email']"));
@@ -54,5 +65,13 @@ public class BaseDriver {
         bekle.until(ExpectedConditions.titleContains("My Account"));
         Assert.assertTrue(driver.getTitle().equals("My Account"), "Login olunamadı");
         System.out.println("Login Testi Bitti");
+        LogEkle.info("Login Testi tamamlandı");
+    }
+
+    @AfterMethod
+    public void AfterMethod(ITestResult sonuc)// tesin sonuç ve isim bilgisini olduğu değişkeni
+    {
+        LogEkle.info(sonuc.getName()+",  Metod Bitti"+" Sonuc="+ (sonuc.getStatus()==1 ? "Passed" : "Failed") );
+        LogEkle.warn("WARN : Metod bitti, hata oluşmuş olsa idi bu şekilde eklenebilir.Bir if kontrolü ile");
     }
 }
